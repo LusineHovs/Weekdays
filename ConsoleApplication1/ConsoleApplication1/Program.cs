3537b465216806b2a8ee0f916be60eecda95243c
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-01-16 13:18:30 +0400

    FIrst commitment.

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeekdaysIndex
+{
+    class Weekdays
+    {
+        private string[] weekdays = new string[7] { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index > weekdays.Length && index<0)
+                    throw new IndexOutOfRangeException();
+                return weekdays[index];
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Weekdays days = new Weekdays();
+
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine($"The {i+1}st day of the week is {days[i]}");
+            }
+        }
+    }
+}
